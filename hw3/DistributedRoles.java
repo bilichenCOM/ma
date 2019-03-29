8cc370ee7e1159336e8d1941bb0372b62af15e0f
Bilichenko Mykhailo <bilichenko.mykhailo@gmail.com>
2019-03-29 13:05:25 +0200

    corrections

--- hw3/DistributedRoles.java
+++ hw3/DistributedRoles.java
@@ -1,15 +1,13 @@
-	public static String printTextPerRole(String[] roles, String[] textLines) {
-		StringBuilder text = new StringBuilder();
-		for(int i = 0; i<roles.length; i++) {
-            text.append(roles[i]+":\n");
-            for(int j = 0; j<textLines.length; j++) {
-                if(textLines[j].contains(roles[i])) {
-                    text.append((j+1)+") ")
-                        .append(textLines[j].replace(roles[i]+": ",""))
-                        .append("\n");
-                }
-            }
-            text.append("\n");
-        }
-		return text.toString();
+public static String printTextPerRole(String[] roles, String[] textLines) {
+	StringBuilder text = new StringBuilder();
+	for (int i = 0; i < roles.length; i++) {
+		text.append(roles[i] + ":\n");
+		for (int j = 0; j < textLines.length; j++) {
+			if (textLines[j].contains(roles[i])) {
+				text.append((j + 1) + ") ").append(textLines[j].replace(roles[i] + ": ", "")).append("\n");
+			}
+		}
+		text.append("\n");
 	}
+	return text.toString();
+}

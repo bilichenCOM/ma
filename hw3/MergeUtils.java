cb403d1a46b4db23054699403b11c6328fdee2dd
Bilichenko Mykhailo <bilichenko.mykhailo@gmail.com>
2019-03-29 12:29:33 +0200

    hw3

---
+++ hw3/MergeUtils.java
@@ -0,0 +1,27 @@
+public static int[] mergeArrays(int[] a1, int[] a2) {
+	final int k = a1.length;
+	final int l = a2.length;
+	int[] merged = new int[k + l];
+	int i = 0;
+	int j = 0;
+	int n = 0;
+	while (i < k || j < l) {
+		if (i < k && j < l) {
+			if (a1[i] <= a2[j]) {
+				merged[n] = a1[i];
+				i++;
+			} else {
+				merged[n] = a2[j];
+				j++;
+			}
+		} else if (i < k) {
+			merged[n] = a1[i];
+			i++;
+		} else if (j < l) {
+			merged[n] = a2[j];
+			j++;
+		}
+		n++;
+	}
+	return merged;
+}

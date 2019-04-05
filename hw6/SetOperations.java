626d4ff2aef52a535e4c01d780ca9c225b02e89a
Bilichenko Mykhailo <bilichenko.mykhailo@gmail.com>
2019-04-06 00:59:30 +0300

    corrections2

--- hw6/SetOperations.java
+++ hw6/SetOperations.java
@@ -7,10 +7,12 @@ public class SetOperations {
 	public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
 		Set<T> result = new HashSet<>();
 		Set<T> temp = new HashSet<>();
+		Set<T> mutableSet1 = new HashSet<>(set1);
+		Set<T> mutableSet2 = new HashSet<>(set2);
 		temp.addAll(set1);
-		set1.removeAll(set2);
+		mutableSet1.removeAll(set2);
 		temp.remove(set1);
-		set2.removeAll(temp);
+		mutableSet2.removeAll(temp);
 		result.addAll(set1);
 		result.addAll(set2);
 		return result;

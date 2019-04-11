66a307a6e55455c80ea7d4f0b09cd5e77ec27e0b
Bilichenko Mykhailo <bilichenko.mykhailo@gmail.com>
2019-04-11 12:47:45 +0300

    comments

--- hw9/StreamUtils.java
+++ hw9/StreamUtils.java
@@ -8,12 +8,27 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamUtils {
+	/*
+	 * Task 1: https://stepik.org/lesson/12781/step/14?unit=3128
+	 * 
+	 */
+
+	// is in hw7
+
+	/*
+	 * Task2: https://stepik.org/lesson/12781/step/12?thread=solutions&unit=3128
+	 * 
+	 */
 	public static <T> void findMinMax(Stream<? extends T> stream, Comparator<? super T> order,
 			BiConsumer<? super T, ? super T> minMaxConsumer) {
 		List<T> list = stream.collect(Collectors.toList());
 		minMaxConsumer.accept(list.stream().min(order).orElse(null), list.stream().max(order).orElse(null));
 	}
 
+	/*
+	 * Task 3: https://stepik.org/lesson/12781/step/11?unit=3128
+	 * 
+	 */
 	public static IntStream pseudoRandomStream(int seed) {
 		return IntStream.iterate(seed, n -> (n * n / 10) % 1000);
 	}
